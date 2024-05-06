6e1733ac5b26f5f4361220bac50f1eddd4379864
dmytro.t <tuxovdmtr@gmail.com>
2024-05-06 18:13:11 +0300

    hw 3 add checking to annotated values

--- Lesson 13. Decorator/HWs/HW 3/Lesson 13 HW 3.py
+++ Lesson 13. Decorator/HWs/HW 3/Lesson 13 HW 3.py
@@ -16,11 +16,12 @@
 
 def check_type(func):  
     def wrapper(*args):
-        for arg in args:
-            if type(arg) is int:
+        input_type = list(func.__annotations__.values())
+        for arg, type in zip(args, input_type):
+            if isinstance(arg, type):
                 continue
             else:
-                raise TypeError(f"{arg} is a {type(arg).__name__} type. Enter an integer.")
+                raise TypeError(f"{arg} is a {arg.__class__.__name__} type. Enter an integer.")
         return func(*args)
     return wrapper    
     
@@ -28,7 +29,10 @@ def check_type(func):
 def add(a: int, b: int) -> int:
     return a + b
 
-print(add(1, 2))
+
+#print(add(1, 2))
+
 add(1, "2")
+print(add.__annotations__)
 add(True, "2")
 add([1, 2], 2)
